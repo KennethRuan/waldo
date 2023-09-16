3a9e5ece582de1d8f0680aace90cce1a03cc7f3e
Maillew <lin.s.william0147@gmail.com>
2023-09-16 11:19:53 -0400

    integrating adhawk, and adding gaze functionality

--- backend/backend/adhawk/gaze.py
+++ backend/backend/adhawk/gaze.py
@@ -1,7 +1,7 @@
 ''' Demonstrates how to subscribe to and handle data from gaze and event streams '''
 
 import time
-
+import numpy as np
 import adhawkapi
 import adhawkapi.frontend
 
@@ -56,7 +56,7 @@ class FrontendData:
         if et_data.pupil_diameter is not None:
             if et_data.eye_mask == adhawkapi.EyeMask.BINOCULAR:
                 rdiameter, ldiameter = et_data.pupil_diameter
-                print(f'Pupil diameter: Left={ldiameter:.2f} Right={rdiameter:.2f}')
+                print(f'Pu  pil diameter: Left={ldiameter:.2f} Right={rdiameter:.2f}')
 
     def _handle_events(self, event_type, timestamp, *args):
         if event_type == adhawkapi.Events.BLINK:
@@ -69,7 +69,7 @@ class FrontendData:
                     self.plane_points.push([self.px,self.py,self.pz])
                     if len(self.plane_points) == 3:
                         self.plane_calibrate = False
-
+        
             self.pblink = timestamp
         
     def _handle_tracker_connect(self):
@@ -88,10 +88,39 @@ class FrontendData:
 
     def find_plane_param(self):
         self.plane_calibrate = True
-        
+        point1 = self.plane_points[0]
+        point2 = self.plane_points[1]
+        point3 = self.plane_points[2]
+
+        # Calculate two vectors on the plane
+        vector1 = point2 - point1
+        vector2 = point3 - point1
+
+        # Calculate the normal vector of the plane using the cross product
+        normal_vector = np.cross(vector1, vector2)
+
+        # The equation of the plane is in the form ax + by + cz + d = 0, so we need to find d
+        # You can use any of the three points to find d
+        self.a, self.b, self.c = normal_vector[0], normal_vector[1], normal_vector[2]
+        self.d = -np.dot(normal_vector, point1)
+
+        # The equation of the plane is now determined
+        print(f"The equation of the plane is {normal_vector[0]}x + {normal_vector[1]}y + {normal_vector[2]}z + {self.d} = 0")
+    
+    def find_closest_point_on_plane(self, pt):
+        # Define the coordinates of the fourth point
+
+        # Calculate the distance from the fourth point to the global plane
+        distance = abs(self.a * pt[0] + self.b * pt[1] + self.c * pt[2] + self.d) / np.sqrt(self.a**2 + self.b**2 + self.c**2)
+
+        # Calculate the point on the plane closest to the fourth point
+        closest_point_on_plane = pt - distance * np.array([self.a, self.b, self.c])
+
+        return closest_point_on_plane
+
     def _handle_tracker_disconnect(self):
         print("Tracker disconnected")
-
+    
 
 def main():
     ''' App entrypoint '''
